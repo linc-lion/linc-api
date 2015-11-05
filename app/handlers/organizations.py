fd131eb3d55c5f9719dc36a55ff7480407da7c1d
Andre Toscano <andre@venidera.com>
2015-11-05 03:03:22 -0200

    organizations adjusted, now it will be tested in heroku

--- app/handlers/organizations.py
+++ app/handlers/organizations.py
@@ -19,7 +19,7 @@ class OrganizationsHandler(BaseHandler):
             query = { 'iid' : int(org_id) }
         except:
             try:
-                query = { 'id' : ObjId(org_id) }
+                query = { '_id' : ObjId(org_id) }
             except:
                 query = { 'name' : org_id}
         query['trashed'] = trashed
@@ -43,16 +43,16 @@ class OrganizationsHandler(BaseHandler):
                 objs = yield self.settings['db'].organizations.find({'trashed':trashed}).to_list(None)
                 self.set_status(200)
                 self.finish(self.json_encode({'status':'success','data':self.list(objs)}))
-
             else:
                 # return a specific organization accepting as id the integer id, hash and name
                 query = self.query_id(org_id,trashed)
-                objs = yield Organization.objects.filter(**query).limit(1).find_all()
-                if len(objs) > 0:
-                    objorg = objs[0].to_son()
-                    objorg['id'] = objs[0].iid
-                    objorg['obj_id'] = str(objs[0]._id)
+                objs = yield self.settings['db'].organizations.find_one(query)
+                if objs:
+                    objorg = objs
+                    objorg['id'] = objs['iid']
+                    objorg['obj_id'] = str(objs['_id'])
                     del objorg['iid']
+                    del objorg['_id']
                     self.set_status(200)
                     self.finish(self.json_encode({'status':'success','data':objorg}))
                 else:
@@ -60,7 +60,6 @@ class OrganizationsHandler(BaseHandler):
                     self.finish(self.json_encode({'status':'error','message':'not found'}))
         else:
             # return a list of organizations
-            #objs = yield Organization.objects.find_all()
             objs = yield self.settings['db'].organizations.find({'trashed':trashed}).to_list(None)
             output = list()
             for x in objs:
@@ -79,21 +78,22 @@ class OrganizationsHandler(BaseHandler):
         # parse data recept by POST and get only fields of the object
         newobj = self.parseInput(Organization)
         # getting new integer id
-        newobj['iid'] = yield Task(self.new_iid,Organization.__collection__)
+        newobj['iid'] = yield Task(self.new_iid,Organization.collection())
         try:
-            neworg = Organization(**newobj)
-            if neworg.validate():
-                # the new object is valid, so try to save
-                try:
-                    newsaved = yield neworg.save()
-                    output = newsaved.to_son()
-                    output['obj_id'] = str(newsaved._id)
-                    # Change iid to id in the output
-                    self.switch_iid(output)
-                    self.finish(self.json_encode({'status':'success','message':'new organization saved','data':output}))
-                except:
-                    # duplicated index error
-                    self.dropError(409,'duplicated name for an organization')
+            neworg = Organization(newobj)
+            neworg.validate()
+            # the new object is valid, so try to save
+            try:
+                newsaved = yield self.settings['db'].organizations.insert(neworg.to_native())
+                output = neworg.to_native()
+                print(output)
+                output['obj_id'] = str(newsaved)
+                # Change iid to id in the output
+                self.switch_iid(output)
+                self.finish(self.json_encode({'status':'success','message':'new organization saved','data':output}))
+            except:
+                # duplicated index error
+                self.dropError(409,'duplicated name for an organization')
         except:
             # received data is invalid in some way
             self.dropError(400,'Invalid input data.')
@@ -103,7 +103,7 @@ class OrganizationsHandler(BaseHandler):
     def put(self, org_id=None):
         # update an organization
         # parse data recept by PUT and get only fields of the object
-        update_data = self.parseInput(Organization)
+        update_data = self.input_data
         fields_allowed_to_be_update = ['name','trashed']
         # validate the input for update
         update_ok = False
@@ -115,33 +115,40 @@ class OrganizationsHandler(BaseHandler):
             query = self.query_id(org_id)
             if 'trashed' in update_data.keys():
                 del query['trashed']
-            #updobj = yield self.settings['db'].organizations.find_one(query)
-            updobj = yield Organization.objects.filter(**query).limit(1).find_all()
-            if len(updobj) > 0:
-                updobj = updobj[0]
+            updobj = yield self.settings['db'].organizations.find_one(query)
+            if updobj:
+                updict = dict()
                 for field in fields_allowed_to_be_update:
                     if field in update_data.keys():
-                        cmd = "updobj."+field+" = "
                         if isinstance(update_data[field],str):
-                            cmd = cmd + "'" + str(update_data[field]) + "'"
+                            updict[field] =  "'" + str(update_data[field]) + "'"
                         else:
-                            cmd = cmd + str(update_data[field])
-                        exec(cmd)
-                updobj.updated_at = datetime.now()
-                try:
-                    if updobj.validate():
+                            updict[field] = str(update_data[field])
+                if True:
+                #try:
+                    if updict:
+                        updict['updated_at'] = datetime.now()
+                        updid = updobj['_id']
                         # the object is valid, so try to save
-                        try:
-                            saved = yield updobj.save()
-                            output = saved.to_son()
-                            output['obj_id'] = str(saved._id)
+                        #try:
+                        if True:
+                            saved = yield self.settings['db'].organizations.update({'_id':updid},{'$set' : updict})
+                            output = updobj
+                            output['obj_id'] = str(updid)
+                            del output['_id']
+                            for k,v in updict.items():
+                                output[k] = v
                             # Change iid to id in the output
                             self.switch_iid(output)
                             self.finish(self.json_encode({'status':'success','message':'organization updated','data':output}))
-                        except:
+                        #except:
+                        else:
                             # duplicated index error
                             self.dropError(409,'duplicated name for an organization')
-                except:
+                    else:
+                        self.dropError(400,'No data provided to be updated')
+                #except:
+                else:
                     # received data is invalid in some way
                     self.dropError(400,'Invalid input data.')
             else:
