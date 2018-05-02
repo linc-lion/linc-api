cc03317c5cd6850e1c0229ca6566bf551c53a4e4
Makoto Kadowaki <makoto@venidera.com>
2018-05-02 17:19:04 -0300

    adjustmentes to export lions data

--- app/handlers/data_export.py
+++ app/handlers/data_export.py
@@ -10,24 +10,36 @@ from tornado.web import asynchronous
 class DataExportHandler(BaseHandler):
     SUPPORTED_METHODS = ('POST')
 
-    def standard_keys(self):
+    def standard_keys(self, animal=False):
         keys = odict()
-        keys['_id'] = 'HashId'
-        keys['iid'] = 'Id'
-        keys['uploading_user_iid'] = 'Uploaded By'
-        keys['notes'] = 'Notes'
-        keys['tags'] = 'Tags'
-        keys['owner_organization_iid'] = 'Owned By Org.'
-        keys['uploading_organization_iid'] = 'Uploaded by Org.'
-        keys['date_stamp'] = 'Datestamp'
-        keys['date_of_birth'] = 'Date of Birth'
-        keys['location'] = 'Location'
-        keys['gender'] = 'Gender'
-        keys['main_image_iid'] = 'Cover image Id'
-        keys['is_verified'] = 'Verified'
-        keys['created_at'] = 'Created'
-        keys['updated_at'] = 'Updated'
-        keys['animal_iid'] = self.animal.capitalize() + ' Id'
+
+        if not animal:
+            keys['_id'] = 'HashId'
+            keys['iid'] = 'Id'
+            keys['uploading_user_iid'] = 'Uploaded By'
+            keys['notes'] = 'Notes'
+            keys['tags'] = 'Tags'
+            keys['owner_organization_iid'] = 'Owned By Org.'
+            keys['uploading_organization_iid'] = 'Uploaded by Org.'
+            keys['date_stamp'] = 'Datestamp'
+            keys['date_of_birth'] = 'Date of Birth'
+            keys['location'] = 'Location'
+            keys['gender'] = 'Gender'
+            keys['main_image_iid'] = 'Cover image Id'
+            keys['is_verified'] = 'Verified'
+            keys['created_at'] = 'Created'
+            keys['updated_at'] = 'Updated'
+            keys['animal_iid'] = self.animal.capitalize() + ' Id'
+        else:
+            keys['_id'] = 'HashId'
+            keys['name'] = 'Lion Name'
+            keys['iid'] = 'Id'
+            keys['organization_iid'] = 'Org. Id'
+            keys['primary_image_set_iid'] = 'Primary ImageSet Id'
+            keys['dead'] = 'Is Dead'
+            keys['created_at'] = 'Created'
+            keys['updated_at'] = 'Updated'
+
         return keys
 
     def check_structure(self, key, data):
@@ -39,7 +51,8 @@ class DataExportHandler(BaseHandler):
 
     @engine
     def get_data(self, idslist=None, animals=False, callback=None):
-        keys = self.standard_keys()
+        keys = self.standard_keys(animals)
+        info(keys)
         # current_user['organization_iid']
         resp = True
         try:
@@ -55,8 +68,10 @@ class DataExportHandler(BaseHandler):
                 obj = cursor.next_object()
                 rowdata = odict()
                 for k, v in keys.items():
-                    rowdata[v] = obj[k]
+                    if k in obj:
+                        rowdata[v] = obj[k]
                 lines.append(rowdata.copy())
+            info(lines)
             resp = {'fnames': fieldnames.copy(), 'lines': lines.copy()}
         except Exception as e:
             info(e)
