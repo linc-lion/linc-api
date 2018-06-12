9b9dce59ab6e84f3ec6892046fa1d1d696eec246
Andre E Toscano <andre@venidera.com>
2018-06-12 19:17:30 -0300

    added the check capabilities about what lion included in the training

--- app/lib/check_cv.py
+++ app/lib/check_cv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # LINC is an open source shared database and facial recognition
 # system that allows for collaboration in wildlife monitoring.
 # Copyright (C) 2016  Wildlifeguardians
@@ -27,23 +28,30 @@ from json import dumps, loads
 
 @gen.coroutine
 def checkresults(db, api):
+    info('=========================================================================')
+    info(' Starting CV Request processing - {}'.format(datetime.now().isoformat()))
+    info('=========================================================================')
     # Check results execute GET in the CV Server URL to acquire results for cv requests
     AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
-    # Clear cvresults without cvrequests
+    info('    Clear cvresults without cvrequests')
     cvreqs = db.cvrequests.find()
     lcvreqids = [x['iid'] for x in cvreqs]
     rmcv = db.cvresults.remove({'cvrequest_iid': {'$nin': lcvreqids}}, multi=True)
-    info(rmcv)
+    info('     {}'.format(rmcv))
     # Get ids with status != finished or error
     cvreqs = db.cvrequests.find({'status': {'$nin': ['finished', 'error']}})
     cvreqs = [x for x in cvreqs]
-    info('CVReqs count: ' + str(len(cvreqs)))
+    info('    CV Request not finished of error - count: ' + str(len(cvreqs)))
     # Check if cvresults exists
     for cvreq in cvreqs:
         cvres = db.cvresults.find_one({'cvrequest_iid': cvreq['iid']})
         if not cvres:
             # Create the CVResults
-            iid = db.counters.find_and_modify(query={'_id': 'cvresults'}, update={'$inc': {'next': 1}}, new=True, upsert=True)
+            iid = db.counters.find_and_modify(
+                query={'_id': 'cvresults'},
+                update={'$inc': {'next': 1}},
+                new=True,
+                upsert=True)
             newcvres = dict()
             newcvres['cvrequest_iid'] = cvreq['iid']
             newcvres['iid'] = iid['next']
@@ -52,54 +60,78 @@ def checkresults(db, api):
             newcvres['created_at'] = dt
             newcvres['updated_at'] = dt
             ncvresobjid = db.cvresults.insert(newcvres)
-            info('Request ID created: ' + str(ncvresobjid))
+            info('CV results created id: ' + str(ncvresobjid))
         # Cvres exists, so try to get data
-        http_client = AsyncHTTPClient()
-        url = api['CVSERVER_URL_RESULTS'] + cvreq['server_uuid']
-        if url == '':
-            info('\nFail to get CVSERVER_URL_RESULTS... Stopping process...')
+        req_body = loads(cvreq['request_body'])
+        info(req_body)
+        resp_cv = loads(cvres['match_probability'])
+        # Check for cv results
+        if req_body.get('classifiers', False):
+            info(' >>> CV Request invalid - id: {}'.format(cvreq['iid']))
+            info(' >>> No classifiers found.')
+        else:
+            info(req_body)
+            if req_body['classifiers'].get('cv', False):
+                info('    Processing call for the classifier CV')
+                for cv_call in req_body['cv_calls']:
+                    info(cv_call)
+
+            info('\n')
+            info(cvreq)
+            info('\n')
+            info(cvres)
+            info('\n')
             return
-        request = HTTPRequest(**{
-            'url': url,
-            'method': 'GET',
-            'auth_username': api['CV_USERNAME'],
-            'auth_password': api['CV_PASSWORD'],
-            'request_timeout': 720,
-            'validate_cert': False
-        })
-        try:
-            dt = datetime.now()
-            info(" ### Checking CV Request: " + str(cvreq['iid']) + " ###")
-            response = yield http_client.fetch(request)
-            info('Connection OK')
-            rbody = json_decode(response.body)
-            rbody['code'] = response.code
-            rbody['reason'] = response.reason
-            # nstatus = rbody['identification']['status']
-            nstatus = rbody['status']
-            if nstatus == 'done':
-                nstatus = 'finished'
-            mresult = dumps(rbody['match_probability'])
-            info(rbody)
-            info('CV Req Status: ' + nstatus)
-            # info('match_probability: '+mresult)
-            db.cvrequests.update({'_id': cvreq['_id']}, {'$set': {'status': nstatus, 'updated_at': dt}})
-            db.cvresults.update({'cvrequest_iid': cvreq['iid']}, {'$set': {'match_probability': mresult, 'updated_at': dt}})
-        except HTTPError as e:
-            info('HTTTP error returned... ')
-            info('Code: ' + str(e.code))
-            info("Message: " + str(e.message))
-            if e.response:
-                info('URL: ' + str(e.response.effective_url))
-                info('Reason: ' + str(e.response.reason))
-                info('Body: ' + str(e.response.body))
-            if int(e.code) == 400 and loads(e.response.body.decode("utf-8"))['status'] == 'error':
-                db.cvrequests.update({'_id': cvreq['_id']}, {'$set': {'status': 'error', 'updated_at': dt}})
-            elif int(e.code) == 401:
-                # authentication error
-                db.cvrequests.update({'_id': cvreq['_id']}, {'$set': {'status': 'authentication failure', 'updated_at': dt}})
-                # communication failure
-                db.cvrequests.update({'_id': cvreq['_id']}, {'$set': {'status': 'network failure', 'updated_at': dt}})
-        except Exception as e:
-            # Other errors are possible, such as IOError.
-            info("Other Errors: " + str(e))
+
+            http_client = AsyncHTTPClient()
+            url = api['CVSERVER_URL_RESULTS'] + cvreq['server_uuid']
+            if url == '':
+                info('\nFail to get CVSERVER_URL_RESULTS... Stopping process...')
+                return
+            request = HTTPRequest(**{
+                'url': url,
+                'method': 'GET',
+                'auth_username': api['CV_USERNAME'],
+                'auth_password': api['CV_PASSWORD'],
+                'request_timeout': 720,
+                'validate_cert': False
+            })
+            try:
+                dt = datetime.now()
+                info(" ### Checking CV Request: " + str(cvreq['iid']) + " ###")
+                response = yield http_client.fetch(request)
+                info('Connection OK')
+                rbody = json_decode(response.body)
+                rbody['code'] = response.code
+                rbody['reason'] = response.reason
+                # nstatus = rbody['identification']['status']
+                nstatus = rbody['status']
+                if nstatus == 'done':
+                    nstatus = 'finished'
+                mresult = dumps(rbody['match_probability'])
+                info(rbody)
+                info('CV Req Status: ' + nstatus)
+                # info('match_probability: '+mresult)
+                db.cvrequests.update({'_id': cvreq['_id']}, {'$set': {'status': nstatus, 'updated_at': dt}})
+                db.cvresults.update({'cvrequest_iid': cvreq['iid']}, {'$set': {'match_probability': mresult, 'updated_at': dt}})
+            except HTTPError as e:
+                info('HTTTP error returned... ')
+                info('Code: ' + str(e.code))
+                info("Message: " + str(e.message))
+                if e.response:
+                    info('URL: ' + str(e.response.effective_url))
+                    info('Reason: ' + str(e.response.reason))
+                    info('Body: ' + str(e.response.body))
+                if int(e.code) == 400 and loads(e.response.body.decode("utf-8"))['status'] == 'error':
+                    db.cvrequests.update({'_id': cvreq['_id']}, {'$set': {'status': 'error', 'updated_at': dt}})
+                elif int(e.code) == 401:
+                    # authentication error
+                    db.cvrequests.update({'_id': cvreq['_id']}, {'$set': {'status': 'authentication failure', 'updated_at': dt}})
+                    # communication failure
+                    db.cvrequests.update({'_id': cvreq['_id']}, {'$set': {'status': 'network failure', 'updated_at': dt}})
+            except Exception as e:
+                # Other errors are possible, such as IOError.
+                info("Other Errors: " + str(e))
+    info('=========================================================================')
+    info(' CV Request processing finished - {}'.format(datetime.now().isoformat()))
+    info('=========================================================================')
