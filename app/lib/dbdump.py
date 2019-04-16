b02e71aebcca9a0618f1551d104ac6adcde4288a
Andre Toscano <andre@venidera.net>
2019-04-15 21:37:02 -0300

    adjusting the dbdump to generate tokens using secrets from standard library; adding the old documentation sample;

--- app/lib/dbdump.py
+++ app/lib/dbdump.py
@@ -25,7 +25,7 @@ from json import dumps
 from time import time
 import zipfile
 from os import listdir, remove
-from uuid import uuid4
+from secrets import token_urlsafe
 
 @coroutine
 def dbdump(db, url_preffix, file_path):
@@ -38,7 +38,7 @@ def dbdump(db, url_preffix, file_path):
             info('Removing file: {}'.format(file_path + filen))
             remove(file_path + filen)
     filename = 'lion-db-dump-' + datetime.utcnow().isoformat()
-    filename = filename.replace(':', '-').split('.')[0] + '-' + str(uuid4())
+    filename = filename.replace(':', '-').split('.')[0] + '-' + token_urlsafe(48)
     info('Creating file: {}'.format(file_path + filename + '.zip'))
     # Get all Imagesets associated with a lion
     imgsets = list(db.imagesets.find({}, {'animal_iid': 1}))
